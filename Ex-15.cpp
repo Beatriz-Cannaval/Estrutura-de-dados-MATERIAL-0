d6580197cda16996e812be9b79fd6213eb18407b
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:34:54 -0300

    Update Ex-15.cpp

--- Ex-15.cpp
+++ Ex-15.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    for (int i = 1; i <= 100; i++)
+        cout << i << endl;
+
     return 0;
 }
