32480226c08bd5cbd0709b54b53ed2010956f9da
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:34:20 -0300

    Update Ex-13.cpp

--- Ex-13.cpp
+++ Ex-13.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    float nota;
+    cin >> nota;
+
+    if (nota >= 6)
+        cout << "Aprovado";
+    else if (nota >= 4)
+        cout << "Recuperacao";
+    else
+        cout << "Reprovado";
+
     return 0;
 }
