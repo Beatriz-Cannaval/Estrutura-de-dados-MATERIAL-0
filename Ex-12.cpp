898105f8245f74bb949c1ed3d07f83f5a6bbbf92
Beatriz-Cannaval <cannavalbeatriz@gmail.com>
2026-04-06 14:34:03 -0300

    Update Ex-12.cpp

--- Ex-12.cpp
+++ Ex-12.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > b && a > c)
+        cout << a;
+    else if (b > c)
+        cout << b;
+    else
+        cout << c;
+
     return 0;
 }
